ebe361402e6ec4ccbb474f91975bd401aae7b2c6
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-10-14 16:52:09 +0200

    added Home Component

--- src/components/HomePage/about/About.jsx
+++ src/components/HomePage/about/About.jsx
@@ -1,7 +1,15 @@
 import "./about.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <img
+        src="src\assets\27-Bellavista-75.jpg"
+        alt="vista sul mare - BellaVista Casa vacanze"
+        className="bellavista-img align-centre"
+      />
+    </div>
+  );
 };
 
 export default About;
--- src/components/HomePage/header/Header.jsx
+++ src/components/HomePage/header/Header.jsx
@@ -3,18 +3,45 @@ import "./header.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
+import { Button } from "react-bootstrap";
 
 function Header() {
   return (
     <>
+      {/* navbar superiore */}
+      <Navbar className="mx-3" expand="lg">
+        <Button variant="primary" className="mx-3">
+          Richiedi preventivo
+        </Button>
+        <Navbar.Text className="mx-3">TEL 0931999734 / WHATSAPP 3342532043 / BELLAVISTA@INFO.IT</Navbar.Text>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text className="mx-3">CHI SIAMO /</Navbar.Text>
+          <Nav.Link href="https://www.instagram.com">
+            <i className="bi bi-instagram mx-3"></i>
+          </Nav.Link>
+          <Nav.Link href="https://www.facebook.com">
+            <i className="bi bi-facebook"></i>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {/* navabar centrale */}
+
       <div className="hero-container">
-        <Navbar expand="lg" className="bg-body-tertiary navbar-centered">
+        <Navbar expand="lg" className=" navbar-centrale rounded ">
           <Navbar.Brand href="#home">
-            <img alt="" src="/img/logo.svg" width="30" height="30" className="d-inline-block align-top" />
+            <img
+              alt=""
+              src="src\assets\00-Bellavista logo.jpg"
+              width="140"
+              height="80"
+              className="d-inline-block align-centre mx-3 rounded"
+            />
             Bella Vista
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav " />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
@@ -28,7 +55,7 @@ function Header() {
               </NavDropdown>
             </Nav>
 
-            <Nav>
+            <Nav className="mx-3">
               <Nav.Link href="https://www.instagram.com">
                 <i className="bi bi-instagram"></i>
               </Nav.Link>
@@ -38,7 +65,31 @@ function Header() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Image src="src/assets/example.jpg" fluid className="hero-image" />
+
+        {/* Carosello di immagini */}
+        <Carousel>
+          <Carousel.Item>
+            <img className="carousel-img d-block w-100" src="src\assets\04-Bellavista-9.jpg" alt="Prima immagine" />
+            <Carousel.Caption>
+              <h3>Prima immagine</h3>
+              <p>Descrizione della prima immagine.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="carousel-img d-block w-100" src="src\assets\01-Bellavista-01.jpg" alt="Seconda immagine" />
+            <Carousel.Caption>
+              <h3>Seconda immagine</h3>
+              <p>Descrizione della seconda immagine.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="carousel-img d-block w-100" src="src\assets\15-Bellavista-40.jpg" alt="Terza immagine" />
+            <Carousel.Caption>
+              <h3>Terza immagine</h3>
+              <p>Descrizione della terza immagine.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     </>
   );
